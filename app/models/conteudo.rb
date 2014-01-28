cb1564876e55b98c80507b83ee2b8dd472c78fde
LMelo <luis.nickelback@gmail.com>
2014-01-28 08:59:02 -0200

    Commit normal

--- app/models/conteudo.rb
+++ app/models/conteudo.rb
@@ -1,3 +1,14 @@
 class Conteudo < ActiveRecord::Base
+
   attr_accessible :ativo, :link, :texto, :tipo, :titulo
+
+  # VALIDACOES
+  validates_presence_of :titulo, :texto, :tipo
+
+  after_initialize :default_values, :if => :new_record?
+
+  def default_values
+    self.ativo = true
+  end
+
 end
